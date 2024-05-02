6e5218f2e5bbde6b6752f15f36327e5debc2963f
marcolaaa <marcos.ribeirorb@gmail.com>
2024-05-02 15:03:00 -0600

    Adding comments

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -59,10 +59,9 @@ const useFetch = () => {
     const retrieveQuotes = async () => {
         // const response = await axios.request(options);
         // const strRespone = JSON.stringify(response.data.candidates[0].content.parts[0].text, null, 2);
-        //console.log('Gemini API response: ', response.data.candidates[0].content.parts[0].text);
+        // TODO remove line 63 and the comments from line 60 and 61.
         const strResponse = "1. \"The greatest glory in living lies not in never falling, but in rising every time we fall.\" - Nelson Mandela\n2. \"The future belongs to those who believe in the beauty of their dreams.\" - Eleanor Roosevelt\n3. \"Happiness is not something ready made. It comes from your own actions.\" - Dalai Lama\n4. \"When life gives you lemons, make lemonade.\" - Dale Carnegie\n5. \"The only person you are destined to become is the person you decide to be.\" - Ralph Waldo Emerson\n6. \"Don't let yesterday take up too much of today.\" - Will Rogers\n7. \"Believe you can and you're halfway there.\" - Theodore Roosevelt\n8. \"A smile is the best makeup anyone can wear.\" - Marilyn Monroe\n9. \"In three words I can sum up everything I've learned about life: it goes on.\" - Robert Frost\n10. \"The sun is a daily reminder that we too can rise again from the darkness, that we too can shine our own light.\" - S. Ajna";
-        // console.log('Gemini API response: ', JSON.stringify(strResponse, null, 2));
-        const formatedResponse = JSON.stringify(strResponse, null, 2) // TODO remove stringfy...
+        const formatedResponse = JSON.stringify(strResponse, null, 2) // TODO remove stringfy.
             .split('\\n')
             .map(item => item.replace(/\d+\.\s/, '')) // remove numbers
             .map(item => item.replace(/^"|"$/g, '')) // extra remove quotes
